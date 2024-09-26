a6f50359831e7f8dd26798b27d1cd939900c350c
Wilbert Arcila <warcilac9@gmail.com>
2024-09-26 16:34:34 -0600

    Arreglando errores

--- docs/practica6/main.js
+++ docs/practica6/main.js
@@ -4,44 +4,204 @@ const $totalCarrito = d.querySelector("#total-carrito");
 const $btnCompra = d.querySelector("#btn-compra");
 const $mensajeCompra = d.querySelector("#mensaje-compra");
 const $carrito = d.querySelector("#carrito");
+const $loader = d.querySelector('#loader');
 
-d.addEventListener("click", function (e) {
-  if (!e.target.matches(".producto")) {
-    return false;
-  }
+let productIDs = [];
 
-  //console.log(e);
-  const $producto = e.target;
-  let nombre = $producto.getAttribute("data-nombre");
-  let precio = parseFloat($producto.getAttribute("data-precio"));
+//add items
+d.addEventListener("click", function(e) {
+    if (!e.target.matches(".AddButton")){
+        return false;
+    }
+    
+    
+    const $product = e.target.parentElement;
+    
+    let id = $product.getAttribute("data-id");
+    let name = $product.getAttribute("data-nombre");
+    let price = parseFloat($product.getAttribute("data-precio"));
+    
+    
+    let subtotal = 0;
 
-  const $itemCarrito = d.createElement("li");
-  $itemCarrito.innerText = `${nombre} - $${precio}`;
 
-  $listaCarrito.appendChild($itemCarrito);
+    for (const element of productIDs) {
+        console.log(element)
+    }
 
-  let totalActual = parseFloat($totalCarrito.innerText);
-  $totalCarrito.innerText = (totalActual + precio).toFixed(2);
+    if (!productIDs.includes(id)){
+        
+        productIDs.push(id);
+        
+   
+        
+        const $itemcart = d.createElement("li");
+        $itemcart.innerHTML = `<div class="Item${id}" data-quant="1">
+        <h3>${name} - $${price}</h3>
+        <div>Cantidad: 
+        <div class="ProductQuantity${id}"> 1 </div>
+        <button class="ModifyButton" data-behaviour="down" data-id="${id}" data-precio="${price}" >-</button>
+        <button class="ModifyButton" data-behaviour="up" data-id="${id}" data-precio="${price}">+</button>
+        <div class="ProductSubtotal${id}">Subtotal: $${price}</div>
+        <button class="EraseButton" data-behaviour="up" data-id="${id}">Quitar del Carrito</button>
+        </div>
+        `;
+        
+        $listaCarrito.appendChild($itemcart);
+    } else {
+
+        let $itemquant = d.querySelector(".Item"+id);
+        let $itemquantdisplay = d.querySelector(".ProductQuantity"+id);
+        let $itemsubtotal = d.querySelector(".ProductSubtotal"+id);
+        let quant = parseFloat($itemquant.getAttribute("data-quant"));
+        quant++;
+        
+        $itemsubtotal.innerText = "Subtotal: $" + quant * price;
+
+        $itemquantdisplay.innerText = quant;
+        $itemquant.setAttribute("data-quant",quant)
+
+        console.log(price);
+    }
+
+    let total = parseFloat($totalCarrito.innerText);
+
+
+
+    $totalCarrito.innerText = (total + price).toFixed(2);
+    
+        
+    
 });
 
-$listaCarrito.addEventListener("click", function (e) {
-  if (e.target.tagName === "LI") {
-    const $item = e.target;
-    $item.remove();
 
-    let precio = parseFloat($item.innerText.split("- $")[1]);
-    //console.log($item.innerText.split("- $"));
+d.addEventListener("click", function(e) {
+    if (!e.target.matches(".ModifyButton")){
+        return false;
+    }
+    
+    
 
-    let totalActual = parseFloat($totalCarrito.innerText);
-    $totalCarrito.innerText = (totalActual - precio).toFixed(2);
-  }
+    const $button = e.target;
+    
+    let id = $button.getAttribute("data-id");
+    
+    console.log(id);
+
+    let $item = d.querySelector(".Item"+id);
+    let $itemquantdisplay = d.querySelector(".ProductQuantity"+id);
+    let $itemsubtotal = d.querySelector(".ProductSubtotal"+id);
+
+
+    let price = parseFloat($button.getAttribute("data-precio"));
+    let quant = parseFloat($item.getAttribute("data-quant"));
+
+    console.log(price)
+
+    let $value = $button.getAttribute("data-behaviour");
+
+    if($value === "up"){
+        quant++;
+        
+        $itemsubtotal.innerText = "Subtotal: $" + quant * price;
+
+        $itemquantdisplay.innerText = quant;
+        $item.setAttribute("data-quant",quant)
+
+        let total = parseFloat($totalCarrito.innerText);
+
+        $totalCarrito.innerText = (total + price).toFixed(2);
+    } else if ($value === "down"){
+        quant--;
+        
+        $itemsubtotal.innerText = "Subtotal: $" + quant * price;
+
+        $itemquantdisplay.innerText = quant;
+        $item.setAttribute("data-quant",quant)
+
+        let total = parseFloat($totalCarrito.innerText);
+        
+
+        $totalCarrito.innerText = (total - price).toFixed(2);
+
+        if (quant == 0){
+            eliminate(id);
+        }
+    }
+
 });
 
-$btnCompra.addEventListener("click", function (e) {
-  console.log($listaCarrito.children);
-  if ($listaCarrito.children.length > 0) {
-    $mensajeCompra.classList.remove("hidden");
-  } else {
-    alert("El carrito está vacío, no se puede realizar la compra.");
-  }
+
+
+d.addEventListener("click", function(e) {
+    if (!e.target.matches(".EraseButton")){
+        return false;
+    }
+    
+    
+
+    const $button = e.target;
+    
+    let id = $button.getAttribute("data-id");
+    
+    console.log(id);
+
+    let $itemsubtotal = d.querySelector(".ProductSubtotal"+id);
+
+    let subtotalmoney = parseFloat($itemsubtotal.innerText.split("$")[1])
+
+    let total = parseFloat($totalCarrito.innerText);
+
+    $totalCarrito.innerText = (total - subtotalmoney).toFixed(2);
+
+    eliminate(id);
+
 });
+
+
+
+$listaCarrito.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI"){
+        const $item = e.target;
+        $item.remove();
+        
+        let price = parseFloat($item.innerText.split("- $")[1]);
+
+        let total = parseFloat($totalCarrito.innerText);
+        
+        $totalCarrito.innerText = (total - price).toFixed(2);
+        
+    }
+})
+
+$btnCompra.addEventListener("click", function(e) {
+    if($listaCarrito.children.length > 0){
+
+        $loader.classList.remove("hidden");
+        setTimeout(() => {
+            $mensajeCompra.classList.remove("hidden");
+            $loader.classList.add("hidden");
+        }, 5000);
+    } else {
+        alert("No hay articulos en el Carrito");
+    }
+});
+
+
+
+
+function eliminate(id){
+    let $item = d.querySelector(".Item"+id);
+    const $product = $item.parentElement;
+    removeItemOnce(productIDs,id);
+    $product.remove();
+    console.log(productIDs);
+}
+
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
